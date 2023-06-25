b508b55e9720ebaaa82f737e8bdfd6da05a7f13e
tensilick <tensilick@proton.me>
2023-06-25 14:46:48 +0000

    Fixed header

--- colorize.py
+++ colorize.py
@@ -31,4 +31,5 @@ def colorize(value, vmin=None, vmax=None, cmap=None):
 
     # normalize
     vmin = tf.reduce_min(value) if vmin is None else vmin
-    vmax = tf.reduce_max(value) if
+    vmax = tf.reduce_max(value) if vmax is None else vmax
+    value = (value - vmin) / (
