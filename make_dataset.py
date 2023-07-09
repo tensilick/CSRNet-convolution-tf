300234d61da7ca2f63c142c5fc7ccc65b97ebb08
tensilick <tensilick@proton.me>
2023-07-09 17:50:42 +0000

    fix select

--- make_dataset.py
+++ make_dataset.py
@@ -36,3 +36,44 @@ def gaussian_filter_density(gt):
         pt2d = np.zeros(gt.shape, dtype=np.float32)
         pt2d[pt[1],pt[0]] = 1.
         if gt_count > 1:
+            sigma = (distances[i][1]+distances[i][2]+distances[i][3])*0.1
+        else:
+            sigma = np.average(np.array(gt.shape))/2./2. #case: 1 point
+        density += scipy.ndimage.filters.gaussian_filter(pt2d, sigma, mode='constant')
+#     print ('done.')
+    return density
+
+
+#set the root to the Shanghai dataset you download
+root = '/home/rishhanth/Documents/gen_codes/CSRNet-tf/ShanghaiTech/'
+
+#now generate the ShanghaiA's ground truth
+part_A_train = os.path.join(root,'part_A/train_data','images')
+part_A_test = os.path.join(root,'part_A/test_data','images')
+part_B_train = os.path.join(root,'part_B/train_data','images')
+part_B_test = os.path.join(root,'part_B/test_data','images')
+path_sets = [part_A_train,part_A_test]
+
+img_paths = []
+for path in path_sets:
+    for img_path in glob.glob(os.path.join(path, '*.jpg')):
+        img_paths.append(img_path)
+
+for img_path in tqdm(img_paths):
+#     print (img_path)
+    mat = io.loadmat(img_path.replace('.jpg','.mat').replace('images','ground-truth').replace('IMG_','GT_IMG_'))
+    lab_path = img_path.replace('.jpg','.npy').replace('images','labels').replace('IMG_','LAB_')
+    img= plt.imread(img_path)
+    k = np.zeros((img.shape[0],img.shape[1]))
+    gt = mat["image_info"][0,0][0,0][0]
+    if not os.path.exists(path=lab_path):
+#         continue
+        for i in range(0,len(gt)):
+            if int(gt[i][1])<img.shape[0] and int(gt[i][0])<img.shape[1]:
+                k[int(gt[i][1]),int(gt[i][0])]=1
+    
+        k = gaussian_filter_density(k)
+        np.save(lab_path,k)
+#     plt.imshow(k, cmap='jet')
+#     plt.show()
+#     with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground_truth'), 'w') as hf:
