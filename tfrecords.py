97bee4b9a09e13e2dab599b83c2b2264cc2b2a5c
tensilick <tensilick@proton.me>
2023-08-10 07:06:25 +0000

    try again...

--- tfrecords.py
+++ tfrecords.py
@@ -31,4 +31,5 @@ def load_image(addr):
     # read an image and resize to (224, 224)
     # cv2 load images as BGR, convert it to RGB
     img = cv2.imread(addr)
-    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CU
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COL
