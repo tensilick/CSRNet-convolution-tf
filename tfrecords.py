0d732c6e891b4570c475ca1d82be6d518cf56f43
tensilick <tensilick@proton.me>
2023-08-10 11:06:05 +0000

    DVBAPI:

--- tfrecords.py
+++ tfrecords.py
@@ -32,4 +32,5 @@ def load_image(addr):
     # cv2 load images as BGR, convert it to RGB
     img = cv2.imread(addr)
     img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
-    img = cv2.cvtColor(img, cv2.COL
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.fl
